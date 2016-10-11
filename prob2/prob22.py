27551186e1bbbe2a238cefeb253964c9047065e8
elnikkis <noahluces@gmail.com>
2016-10-11 17:20:43 +0900

    Solve 22

--- prob2/prob22.py
+++ prob2/prob22.py
@@ -6,7 +6,6 @@ import re
 # [[Category:英連邦王国|*]]
 
 for line in sys.stdin:
-    m = re.match('^\[\[Category:(.*)(\|.*)?\]\]$', line.strip())
-    print(m.group(1))
-    print(m.groups)
+    m = re.match(r'^\[\[Category:(.*)\]\]$', line.strip())
+    print(m.group(1).split('|')[0])
     pass
