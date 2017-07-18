6438be24b1486ace4f88b90789f4901f20010229
elnikkis <noahluces@gmail.com>
2017-07-18 22:12:52 +0900

    Solve 23

--- prob2/prob23.py
+++ prob2/prob23.py
@@ -3,7 +3,10 @@
 import sys
 import re
 
+# less uk.txt | grep -e '^=' | python prob23.py
+
 for line in sys.stdin:
-    m = re.match(r'^(=+)(.*)(=+)$', line.strip())
-    print(m.group(1).split('|')[0])
-    pass
+    m = re.match(r'^(=+)(.*)(\1)$', line.strip())
+    section_tag = m.group(1).split('|')[0]
+    level = len(section_tag) - 1
+    print(m.group(2), level)
