988d5092c989c74ef76f857c4093097ec3eb9460
elnikkis <noahluces@gmail.com>
2018-02-11 03:37:05 +0900

    Solved 05
    ただし1つの関数では無理だった。

--- rust/prob05.rs
+++ rust/prob05.rs
@@ -1,23 +1,36 @@
-fn bigram<T: Iterator>(l: T) -> Iterator<Item=(T::Item, T::Item)> {
-    let i1 = &l;
-    let i2 = l.skip(1);
-    i1.zip(i2)
+fn bigram_word(sentence: &str) -> Vec<(&str, &str)> {
+    sentence.split_whitespace().zip(
+        sentence.split_whitespace().skip(1)).collect()
+}
+fn bigram_char(sentence: &str) -> Vec<(char, char)> {
+    sentence.chars().zip(
+        sentence.chars().skip(1)).collect()
 }
 
-fn abigram<T: Iterator>(l: T) -> Vec<(T::Item, T::Item)> {
-    let mut res: (T::Item, T::Item) = Vec::new();
-    let mut prev = &l.nth(0).unwrap();
-    for i in l {
-        res.push((*prev, i));
-        let prev = &i;
-    }
-    return res
-}
+// impl traitがrustに実装されていないので、interfaceみたいなIteratorを返す関数というのは書けない
+// なのでVecを返すようにしようとしたけど、今度はVec<char>をVec<&str>に変換するのができない
+//fn bigram(list: Vec<String>) -> Vec<(String, String)> {
+//    let prev = list[0];
+//
+//    let mut ret = Vec::new();
+//    for v in list.into_iter().skip(1) {
+//        ret.push((prev, v));
+//        //ret.push(v);
+//    }
+//    ret
+//}
 
 fn main() {
     let s = "I am an NLPer";
-    let w_bigram = bigram(s.split_whitespace());
-    let c_bigram = bigram(s.chars());
+    let w_bigram = bigram_word(&s);
+    let c_bigram = bigram_char(&s);
+
+    //let w_bigram = bigram(s.split_whitespace().collect());
+    //let c_bigram = bigram(s.chars().map(|c| &(c.to_string())).collect());
+
+    //let w_bigram: Vec<_> = s.split_whitespace().zip(s.split_whitespace().skip(1)).collect();
+    //let c_bigram: Vec<_> = s.chars().zip(s.chars().skip(1)).collect();
+
     println!("単語 bi-gram: {:?}", w_bigram);
     println!("文字 bi-gram: {:?}", c_bigram);
 }
